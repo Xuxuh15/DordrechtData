e619c5baa791eb05cc77536fe57b0ae637e10123
Xuxuh15 <xuxuhxv@gmail.com>
2025-03-27 20:34:41 +0100

    Initial commit

---
+++ src/config.py
@@ -0,0 +1,17 @@
+
+
+from enum import Enum
+
+TEAM = 1
+SEASON = '2024/2025'
+
+class MatchType(Enum):
+    COMPETITION = 'Competition'
+    FRIENDLY = 'Friendly'
+    U21 = 'u21'
+
+class GPSVERSION(Enum):
+    JOHAN =  1
+    CATAPULT = 10
+
+
